a281b7cb61fb7c7cd45b116ab2573044a5edc3c8
HaseU-git <64348058+HaseU-git@users.noreply.github.com>
2022-03-02 13:46:38 +0900

    understand how to srite pixcels

--- test_minilibx/main.c
+++ test_minilibx/main.c
@@ -1,13 +1,45 @@
 #include <mlx.h>
+#include <stdio.h>
+
+typedef struct	s_data {
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+}	t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
 
 int	main(void)
 {
 	void	*mlx;
-	void	*window;
+	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
-	window = mlx_new_window(mlx, 1000, 1000, "Title");
-
+	mlx_win = mlx_new_window(mlx, 500, 500, "Hello world!");
+	img.img = mlx_new_image(mlx, 500, 500);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	int i = 0;
+	while(i < 250)
+	{
+		int j = 0;
+		while (j < 250)
+		{
+			my_mlx_pixel_put(&img, i, j, 0x00FFFFFF);
+			j++;
+		}
+		i++;
+	}
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
 	mlx_loop(mlx);
-	return(0);
+	return (0);
 }
